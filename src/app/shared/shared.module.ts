6688d112ef81f85fdb0db9874c8a54e0949949d5
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-28 09:46:57 +0200

    shared.module ADD declarations AND imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    Error404PageComponent
+  ],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    MaterialModule
   ]
 })
 export class SharedModule { }
