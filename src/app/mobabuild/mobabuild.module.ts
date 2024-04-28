d4514b208e56f4e7415cba7b4230d47d465bb1b1
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-28 09:25:21 +0200

    mobabuild CREATE

---
+++ src/app/mobabuild/mobabuild.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MobabuildRoutingModule } from './mobabuild-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MobabuildRoutingModule
+  ]
+})
+export class MobabuildModule { }
