4aea8833e07a1f743f83b9c70294fef6c8444c9a
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-04-28 09:46:36 +0200

    app-routing.module ADD route Error 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '404', component: Error404PageComponent },
+  { path: '**', redirectTo: '404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
