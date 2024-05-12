97b5707b9e18c6b82e0bbe4e88a2da3b9c6bc512
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-12 21:22:16 +0200

    spell.ts CREATE

---
+++ src/app/shared/interfaces/spell.ts
@@ -0,0 +1,19 @@
+import { Build } from "./build";
+
+export interface SpellSet {
+  id: number;
+  name: string;
+  build: Build;
+  spells: Spell[];
+}
+
+export interface Spell {
+  id: number;
+  name: string;
+  champion_level: number;
+  game_mode: string;
+  description: string;
+  cooldown: string;
+  image: ArrayBuffer;
+  spellSets: SpellSet[];
+}
