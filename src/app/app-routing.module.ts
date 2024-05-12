2dee4c85670b55ac851526bd1f7768fabfb94781
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-12 21:16:46 +0200

    app-routing.module.ts ADD route inicio AND admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
 const routes: Routes = [
+  {
+    path: 'inicio',
+    loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+  },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: '404', component: Error404PageComponent },
   { path: '**', redirectTo: '404' },
 ];
