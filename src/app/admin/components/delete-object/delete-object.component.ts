5cacd1a05cae28bfd16928b1c72b2a594b603bcf
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-13 16:23:39 +0200

    delete-object.component.html CREATE component

---
+++ src/app/admin/components/delete-object/delete-object.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-object',
+  templateUrl: './delete-object.component.html',
+  styles: [
+  ]
+})
+export class DeleteObjectComponent {
+
+}
