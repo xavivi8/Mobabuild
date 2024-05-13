eda2894c378c0ed68dd80d5af0efb4d6ade33fe4
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-13 21:44:43 +0200

    object.service.ts CHANGES

--- src/app/admin/services/object.service.ts
+++ src/app/admin/services/object.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { URL_API } from 'src/environments/environments';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { ObjectD } from 'src/app/shared/interfaces/object';
 
 @Injectable({
@@ -11,6 +11,7 @@ import { ObjectD } from 'src/app/shared/interfaces/object';
 export class ObjectService {
 
   private urlMobabuild: string = `${URL_API}/object`;
+  objectds: ObjectD[] = [];
 
   constructor(private httpClient: HttpClient) { }
 
@@ -31,30 +32,25 @@ export class ObjectService {
   }
 
   getObjectById(id: number): Observable<ObjectD> {
-    return this.httpClient.get<ObjectD>(`${this.urlMobabuild}/findById/${id}`).pipe(
+    return this.httpClient.get<ObjectD>(`${this.urlMobabuild}/findById/${id}`)
+  }
+
+
+  deleteObjectById(id: number): Observable<boolean> {
+    return this.httpClient.delete<any>(`${this.urlMobabuild}/deleteById/${id}`).pipe(
+      map(response => {
+        // Verificar si la respuesta es "ok" para devolver true, de lo contrario devolver false
+        return response === "ok";
+      }),
       catchError(error => {
-        console.error('Error al obtener objeto por ID:', error);
-        return throwError('Error al obtener objeto por ID');
+        console.error('Error al eliminar el objeto:', error);
+        return of(false);
       })
     );
   }
 
-
-  deleteObjectById(id: number): Observable<number> {
-    return this.httpClient.get<number>(`${this.urlMobabuild}/deleteById/${id}`).pipe(
-      catchError(error => {
-        console.error('Error al eliminar objeto por ID:', error);
-        return throwError('Error al eliminar objeto por ID');
-      })
-    );
-  }
 
   setObject(name: string): Observable<number> {
-    return this.httpClient.get<number>(`${this.urlMobabuild}/setObject/${name}`).pipe(
-      catchError(error => {
-        console.error('Error al establecer objeto:', error);
-        return throwError('Error al establecer objeto');
-      })
-    );
+    return this.httpClient.get<number>(`${this.urlMobabuild}/setObject/${name}`)
   }
 }
