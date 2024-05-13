f3d64569d9f298ebae692b11c188089bbb31f853
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-13 16:25:47 +0200

    edit-object.componen CREATE component

---
+++ src/app/admin/components/edit-object/edit-object.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-object',
+  templateUrl: './edit-object.component.html',
+  styles: [
+  ]
+})
+export class EditObjectComponent {
+
+}
