a8d69b2fe1d3a7567d32fd493d9b13522f12e3ab
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-13 16:23:24 +0200

    add-object.component.html CREATE component

---
+++ src/app/admin/components/add-object/add-object.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-object',
+  templateUrl: './add-object.component.html',
+  styles: [
+  ]
+})
+export class AddObjectComponent {
+
+}
