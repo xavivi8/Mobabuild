43bdc12f86a1d831c91b4d0097962c4ca3dd216d
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-21 17:26:39 +0200

    user.ts CHANGE

--- src/app/shared/interfaces/user.ts
+++ src/app/shared/interfaces/user.ts
@@ -1,11 +1,23 @@
 import { Build, FavoriteBuild } from "./build";
 
+export interface Authority {
+  id: number;
+  name: AuthorityName;
+}
+
+export enum AuthorityName {
+  READ = 'READ',
+  WRITE = 'WRITE',
+  ADMIN = 'ADMIN'
+}
+
 export interface User {
   id: number;
   email: string;
-  userName: string;
+  user_name: string;
   pass: string;
-  image: ArrayBuffer;
+  image: ArrayBuffer | null;
   builds: Build[];
   favoriteBuild: FavoriteBuild;
+  authorities: Authority[];
 }
