f5e1ae8ea2d5d77befc9327a4f83ddd208ec3d06
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-21 17:26:31 +0200

    admin-routing.module.ts CHANGES

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ListObjectPageComponent } from './pages/list-object-page/list-object-page.component';
+import { ListUserPageComponent } from './pages/list-user-page/list-user-page.component';
 
 const routes: Routes = [
 
   {
     path: '', component: HomePageComponent,
     children: [
-      { path: 'object', component: ListObjectPageComponent }
+      { path: 'object', component: ListObjectPageComponent },
+      { path: 'user', component: ListUserPageComponent }
     ]
   }
 
