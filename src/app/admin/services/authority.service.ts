b33818b2048f0034ed635d9baa057228e17f14da
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-25 11:55:45 +0200

    authority.service.ts ADD headers

--- src/app/admin/services/authority.service.ts
+++ src/app/admin/services/authority.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Authority } from "src/app/shared/interfaces/user";
@@ -10,10 +10,22 @@ import { URL_API } from "src/environments/environments";
 export class AuthorityService {
   private urlMobabuild: string = `${URL_API}/authority`;
 
+  private username = 'usuarioReadWrite@gmail.com';
+  private password = 'UsuarioReadWritePass1';
+  private authHeader = `Basic ${btoa(`${this.username}:${this.password}`)}`;
+
+  private authHeaderWithJson = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': this.authHeader
+    }),
+    withCredentials: true
+  };
+
   constructor(private http: HttpClient) { }
 
   findAll(): Observable<Authority[]> {
-    return this.http.get<Authority[]>(this.urlMobabuild);
+    return this.http.get<Authority[]>(`${this.urlMobabuild}/findAll`, this.authHeaderWithJson);
   }
 
 }
