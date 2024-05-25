1d2ef839b52236e64350811c445583b7d19a4f16
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-25 18:20:57 +0200

    list-spell-page CREATE

---
+++ src/app/admin/pages/list-spell-page/list-spell-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-spell-page',
+  templateUrl: './list-spell-page.component.html',
+  styleUrls: ['./list-spell-page.component.css']
+})
+export class ListSpellPageComponent {
+
+}
