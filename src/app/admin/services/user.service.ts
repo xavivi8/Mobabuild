4635a80c60c8671bbe80535e0cd00158ba35748a
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-25 11:55:19 +0200

    user.service CHANGES

--- src/app/admin/services/user.service.ts
+++ src/app/admin/services/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { AddUserRequest, User } from "src/app/shared/interfaces/user";
+import { SharedService } from "src/app/shared/service/shared.service";
 import { URL_API } from "src/environments/environments";
 
 @Injectable({
@@ -11,37 +12,37 @@ export class UserService {
 
   private urlMobabuild: string = `${URL_API}/user`;
 
-  private username = 'usuarioReadWrite@gmail.com';
-  private password = 'UsuarioReadWritePass1';
-  private authHeader = `Basic ${btoa(`${this.username}:${this.password}`)}`;
+  constructor(
+    private httpClient: HttpClient,
+    private sharedService: SharedService
+  ) {
 
-  private authHeaderWithJson = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': this.authHeader
-    }),
-    withCredentials: true
-  };
-
-  constructor(private httpClient: HttpClient) { }
+  }
 
   findAll(): Observable<User[]> {
-    return this.httpClient.get<User[]>(`${this.urlMobabuild}/findAll`, this.authHeaderWithJson);
+    return this.httpClient.get<User[]>(`${this.urlMobabuild}/findAll`, this.sharedService.getAuthHeaderWithJson());
   }
 
   findById(id: number): Observable<User> {
-    return this.httpClient.get<User>(`${this.urlMobabuild}/find/${id}`);
+    return this.httpClient.get<User>(`${this.urlMobabuild}/find/${id}`, this.sharedService.getAuthHeaderWithJson());
   }
 
   updateUser(user: User): Observable<User> {
-    return this.httpClient.post<User>(`${this.urlMobabuild}/update`, JSON.stringify(user), this.authHeaderWithJson );
+    console.log(user);
+
+    return this.httpClient.put<User>(`${this.urlMobabuild}/update`, JSON.stringify(user), this.sharedService.getAuthHeaderWithJson() );
   }
 
+  /**
+   * @description Add a new user
+   * @param {AddUserRequest} addUserRequest
+   * @returns {User} Observable<User>
+   */
   addUserWithoutImage(addUserRequest: AddUserRequest): Observable<User> {
     console.log(addUserRequest);
 
     console.log(JSON.stringify(addUserRequest));
 
-    return this.httpClient.post<User>(`${this.urlMobabuild}/add`, JSON.stringify(addUserRequest), this.authHeaderWithJson);
+    return this.httpClient.post<User>(`${this.urlMobabuild}/add`, JSON.stringify(addUserRequest), this.sharedService.getAuthHeaderWithJson());
   }
 }
