89493109be05b659292cc4a607101cec941d0ef0
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-25 12:03:13 +0200

    app.module.ts ADD DeleteUserComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ObjectService } from './admin/services/object.service';
 import { AuthInterceptor } from './shared/interceptors/auth.service';
 import { MaterialModule } from './material/material.module';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeleteUserComponent
   ],
   imports: [
     BrowserModule,
