0fe1eca6072ecd11e5eb7039784cd07d1e0de5ae
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-25 11:56:09 +0200

    shared.service.ts ADD header

--- src/app/shared/service/shared.service.ts
+++ src/app/shared/service/shared.service.ts
@@ -7,17 +7,38 @@ export class SharedService {
   private password = 'UsuarioReadWritePass1';
   private authHeader = `Basic ${btoa(`${this.username}:${this.password}`)}`;
 
-  private authHeaderWithJson = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': this.authHeader
-    }),
-    withCredentials: true
-  };
+  authHeaderWithJson;
 
 
   constructor(
     private http: HttpClient,
-  ) {}
+  ) {
+    this.authHeaderWithJson = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': this.authHeader
+      }),
+      withCredentials: true
+    };
+  }
+
+  getAuthHeaderWithJson(){
+    return this.authHeaderWithJson;
+  }
+
+
+  private getAuthHeader(): { headers: HttpHeaders, withCredentials: boolean } {
+    const username = this.username;
+    const password = this.password;
+    const authHeader = `Basic ${btoa(`${username}:${password}`)}`;
+
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': authHeader
+      }),
+      withCredentials: true
+    };
+  }
 
 }
