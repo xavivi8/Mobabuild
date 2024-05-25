f30cc595b5212c9f34e73b33fa5e53fdaf6d65ab
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-25 18:21:11 +0200

    list-rune-page. CREATE

---
+++ src/app/admin/pages/list-rune-page/list-rune-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-rune-page',
+  templateUrl: './list-rune-page.component.html',
+  styleUrls: ['./list-rune-page.component.css']
+})
+export class ListRunePageComponent {
+
+}
