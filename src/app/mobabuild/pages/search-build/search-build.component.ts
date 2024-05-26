83117ecc8a7f903c95e442601cac8d0f681154ff
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-26 18:37:39 +0200

    search-build CREATE

---
+++ src/app/mobabuild/pages/search-build/search-build.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-build',
+  templateUrl: './search-build.component.html',
+  styleUrls: ['./search-build.component.css']
+})
+export class SearchBuildComponent {
+
+}
