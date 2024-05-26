936d4b9cc4b7b5b4a6fbd0d0e29b03dd40a634f7
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-26 11:20:38 +0200

    delete-spell CREATE

---
+++ src/app/admin/components/delete-spell/delete-spell.component.ts
@@ -0,0 +1,57 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Spell } from 'src/app/shared/interfaces/spell';
+import { SpellService } from '../../services/spell.service';
+import { CLOSE } from 'src/app/shared/interfaces/messages';
+
+@Component({
+  selector: 'app-delete-spell',
+  templateUrl: './delete-spell.component.html',
+  styleUrls: ['./delete-spell.component.css']
+})
+export class DeleteSpellComponent {
+
+  /**
+   * @xavivi8
+   * @description inicializa el componente
+   * @param {MatDialogRef<DeleteSpellComponent>} dialogRef
+   * @param {Spell} spell
+   * @param {SpellService} spellService
+   * @param {MatSnackBar} snackBar
+   */
+  constructor(
+    public dialogRef: MatDialogRef<DeleteSpellComponent>,
+    @Inject(MAT_DIALOG_DATA) public spell: Spell,
+    private spellService: SpellService,
+    private snackBar: MatSnackBar,
+  ) { }
+
+  /**
+   * @xavivi8
+   * @description elimina el spell
+   */
+  async deleteChampion() {
+    try {
+      const success = await this.spellService.delete(this.spell).toPromise();
+
+      if (success) {
+        this.snackBar.open('El hechizo se eliminó correctamente.', CLOSE, { duration: 5000 });
+        this.dialogRef.close({ ok: true, data: 'ok' });
+      } else {
+        this.snackBar.open('No se pudo eliminar el hechizo.', CLOSE, { duration: 5000 });
+      }
+    } catch (error) {
+      console.error('Error al eliminar el hechizo:', error);
+      this.snackBar.open('Ocurrió un error al eliminar el hechizo.', CLOSE, { duration: 5000 });
+    }
+  }
+
+  /**
+   * Metodo para cerrar el dialog
+   */
+  onNoClick() {
+    this.dialogRef.close({ ok: false });
+  }
+
+}
