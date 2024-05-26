b91c5952a2a50827229510590432d38058f3bd4b
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-26 22:33:59 +0200

    mobabuild.module.ts ADD declarations

--- src/app/mobabuild/mobabuild.module.ts
+++ src/app/mobabuild/mobabuild.module.ts
@@ -5,12 +5,18 @@ import { MobabuildRoutingModule } from './mobabuild-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MaterialModule } from '../material/material.module';
 import { SearchBuildComponent } from './pages/search-build/search-build.component';
+import { DeleteBuildComponent } from './components/delete-build/delete-build.component';
+import { AddBuildPageComponent } from './pages/add-build-page/add-build-page.component';
+import { EditBuildPageComponent } from './pages/edit-build-page/edit-build-page.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    SearchBuildComponent
+    SearchBuildComponent,
+    DeleteBuildComponent,
+    AddBuildPageComponent,
+    EditBuildPageComponent,
   ],
   imports: [
     CommonModule,
