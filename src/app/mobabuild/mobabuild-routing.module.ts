5978bc68a47e821cca539fe6777d1acd10121240
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-26 14:31:30 +0200

    mobabuild-routing.module.ts ADD routes

--- src/app/mobabuild/mobabuild-routing.module.ts
+++ src/app/mobabuild/mobabuild-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomePageComponent,
+    children: [
+      { path: 'home_page', component: HomePageComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
