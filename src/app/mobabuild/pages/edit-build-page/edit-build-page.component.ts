782067217b91db23eb77cb9d57e24b25387788fb
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-26 22:33:17 +0200

    CREATE edit-build-page.component

---
+++ src/app/mobabuild/pages/edit-build-page/edit-build-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-build-page',
+  templateUrl: './edit-build-page.component.html',
+  styleUrls: ['./edit-build-page.component.css']
+})
+export class EditBuildPageComponent {
+
+}
