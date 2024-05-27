acd22d218c47302e84cd93d8ddb01212d06a39e6
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-27 22:36:36 +0200

    build.service.ts ADD create

--- src/app/mobabuild/service/build.service.ts
+++ src/app/mobabuild/service/build.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Build } from "src/app/shared/interfaces/build";
 import { SharedService } from "src/app/shared/service/shared.service";
 import { URL_API } from "src/environments/environments";
 
@@ -13,4 +15,8 @@ export class BuildService {
     private sharedService: SharedService,
     private httpClient: HttpClient
   ) { }
+
+  create(build: Build): Observable<Build> {
+    return this.httpClient.post<Build>(`${this.urlMobabuild}/create`, JSON.stringify(build), this.sharedService.getAuthHeaderWithJson());
+  }
 }
