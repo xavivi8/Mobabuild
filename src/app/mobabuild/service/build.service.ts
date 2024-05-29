bd7f43960e7fa67ef273161356813b1c7901411a
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-29 20:44:30 +0200

    build ADD findByChampion, findAll AND update

--- src/app/mobabuild/service/build.service.ts
+++ src/app/mobabuild/service/build.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Build } from "src/app/shared/interfaces/build";
+import { Champions } from "src/app/shared/interfaces/champions";
 import { SharedService } from "src/app/shared/service/shared.service";
 import { URL_API } from "src/environments/environments";
 
@@ -16,7 +17,21 @@ export class BuildService {
     private httpClient: HttpClient
   ) { }
 
+  findAll(): Observable<Build[]> {
+    return this.httpClient.get<Build[]>(this.urlMobabuild, this.sharedService.getAuthHeaderWithJson());
+  }
+
+  findByChampion(champion: Champions): Observable<Build[]> {
+    return this.httpClient.post<Build[]>(`${this.urlMobabuild}/findByChampion`, JSON.stringify(champion), this.sharedService.getAuthHeaderWithJson());
+  }
+
   create(build: Build): Observable<Build> {
     return this.httpClient.post<Build>(`${this.urlMobabuild}/create`, JSON.stringify(build), this.sharedService.getAuthHeaderWithJson());
   }
+
+  update(build: Build): Observable<Build> {
+    return this.httpClient.put<Build>(`${this.urlMobabuild}/update`, JSON.stringify(build), this.sharedService.getAuthHeaderWithJson());
+  }
+
+
 }
