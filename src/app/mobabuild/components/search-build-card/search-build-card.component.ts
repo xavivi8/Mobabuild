3195f54e1cccd280be486cd4f96971734530804f
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-29 20:41:48 +0200

    CREATE search-build-card

---
+++ src/app/mobabuild/components/search-build-card/search-build-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-build-card',
+  templateUrl: './search-build-card.component.html',
+  styleUrls: ['./search-build-card.component.css']
+})
+export class SearchBuildCardComponent {
+
+}
