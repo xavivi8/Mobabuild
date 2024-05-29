f9bd46d7bc2e443ddbd0945bdd5aca59019220d2
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-29 20:44:42 +0200

    mobabuild-routing.module.ts ADD routing

--- src/app/mobabuild/mobabuild-routing.module.ts
+++ src/app/mobabuild/mobabuild-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AddBuildPageComponent } from './pages/add-build-page/add-build-page.component';
 import { EditBuildPageComponent } from './pages/edit-build-page/edit-build-page.component';
+import { SearchBuildComponent } from './pages/search-build/search-build.component';
+import { ViewBuildPageComponent } from './pages/view-build-page/view-build-page.component';
 
 const routes: Routes = [
   {
@@ -11,6 +13,8 @@ const routes: Routes = [
       { path: 'home_page', component: HomePageComponent },
       { path: 'add_build', component: AddBuildPageComponent },
       { path: 'edit_build', component: EditBuildPageComponent },
+      { path: 'search_build', component: SearchBuildComponent },
+      { path: 'view_build/:id', component: ViewBuildPageComponent }
     ]
   }
 ];
