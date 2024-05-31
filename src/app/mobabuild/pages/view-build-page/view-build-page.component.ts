7ead7de87e0c9199c658a392611f2ebc1471d411
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-05-31 17:07:00 +0200

    view-build-page CHANGES

--- src/app/mobabuild/pages/view-build-page/view-build-page.component.ts
+++ src/app/mobabuild/pages/view-build-page/view-build-page.component.ts
@@ -7,6 +7,8 @@ import { Champions } from 'src/app/shared/interfaces/champions';
 import { SpellSet } from 'src/app/shared/interfaces/spell';
 import { ObjectD, ObjectSet } from 'src/app/shared/interfaces/object';
 import { RuneSet } from 'src/app/shared/interfaces/rune';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CLOSE } from 'src/app/shared/interfaces/messages';
 
 @Component({
   selector: 'app-view-build-page',
@@ -22,11 +24,30 @@ export class ViewBuildPageComponent implements OnInit{
     objects: [],
   }
 
+  public lenghtRuneSet: number[] = [];
+  public lenghtSpellSet: number = 0;
+  public lenghtObjectSet: number = 0;
+
   public indexRuneSet: number = 0;
   public indexSpellSet: number = 0;
   public indexObjectSet: number = 0;
 
+  public usingRuneSet: RuneSet = {
+    id: null,
+    name: 'No hay runas',
+    main_rune: '',
+    main_sub_rune: '',
+    secondary_rune: '',
+    secondary_sub_rune: '',
+    additional_advantages: '',
+    build: null
+  };
+  public usingSpellSet!: SpellSet;
+  public usingObjectSet!: ObjectD;
+
   public champion!: Champions;
+
+
   public spellSet: SpellSet[] = [];
   public objectSet: ObjectSet[] = [];
   public ruenSet: RuneSet[] = [];
@@ -35,6 +56,7 @@ export class ViewBuildPageComponent implements OnInit{
     private buildService: BuildService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
+    public snackBar: MatSnackBar,
   ) {}
 
   ngOnInit(): void {
@@ -45,9 +67,14 @@ export class ViewBuildPageComponent implements OnInit{
 
       this.build = build
       this.getData(this.build);
+      this.takeLenghtRuneSet();
+      this.initBuild();
       return;
     })
 
+  }
+  initBuild() {
+    this.useRuneSet(0);
   }
 
   getData(build: Build) {
@@ -57,5 +84,47 @@ export class ViewBuildPageComponent implements OnInit{
     this.objectSet = build.objectSet
     this.ruenSet = build.runeSet
     this.firstObject = this.objectSet[0]
+  }
+
+  previousRune(){
+    if(this.indexRuneSet >= 0){
+      this.indexRuneSet -= 1
+    } else {
+      this.snackBar.open('Esta es la última runa', CLOSE, {
+        duration: 3000
+      });
+    }
+  }
+
+  nextRune(){
+    if(this.indexRuneSet < this.ruenSet.length - 1){
+      this.indexRuneSet += 1
+    } else {
+      this.snackBar.open('Esta es la última runa', CLOSE, {
+        duration: 3000
+      });
+    }
+  }
+
+  takeLenghtRuneSet(){
+    const lenghtRune = this.ruenSet.length
+
+    for(let i = 0; i < lenghtRune; i++){
+      this.lenghtRuneSet.push(i)
+    }
+  }
+
+  takeLenghtSpellSet(){
+    this.lenghtSpellSet = this.spellSet.length
+  }
+
+  takeLenghtObjectSet(){
+    this.lenghtObjectSet = this.objectSet.length
+  }
+
+  useRuneSet(pos: number){
+    debugger
+    this.usingRuneSet = this.ruenSet[pos]
+    debugger
   }
 }
