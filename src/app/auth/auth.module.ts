246800048f1f9685b7fd6cfae86c8dddc8f1958c
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-01 13:01:56 +0200

    auth.module.ts ADD declarations

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterPageComponent,
+    LoginPageComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
