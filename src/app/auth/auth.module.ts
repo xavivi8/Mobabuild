a3a0229f5bcdc0b7c306791e6559d58bfee2bbeb
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-01 17:11:07 +0200

    auth.module DELETE HomePageComponent

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MaterialModule } from '../material/material.module';
 
 
@@ -12,7 +11,6 @@ import { MaterialModule } from '../material/material.module';
   declarations: [
     RegisterPageComponent,
     LoginPageComponent,
-    HomePageComponent
   ],
   imports: [
     CommonModule,
