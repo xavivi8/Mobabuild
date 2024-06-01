4a89fb9009ab85cfa8fd865943aaa6fc2733c1d8
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-01 17:11:21 +0200

    login-page.component CHANGE

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from '../../service/user.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserLogin } from '../../interfaces/UserLogin';
+import { User } from 'src/app/shared/interfaces/user';
+import { CLOSE } from 'src/app/shared/interfaces/messages';
 
 @Component({
   selector: 'app-login-page',
@@ -16,6 +20,7 @@ export class LoginPageComponent implements OnInit{
   constructor(
     private userService: UserService,
     private snackBar: MatSnackBar,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -25,16 +30,41 @@ export class LoginPageComponent implements OnInit{
     })
   }
 
-  login() {
+  async login() {
+    debugger
+    if (this.userForm.valid) {
+      const userLogin: UserLogin = {
+        email: this.userForm.get('email')?.value,
+        pass: this.userForm.get('pass')?.value
+      };
 
+      try {
+        const RESPONSE = await this.userService.login(userLogin);
+        debugger
+        if (RESPONSE) {
+          const USER: User = RESPONSE;
+          localStorage.setItem('user', JSON.stringify(USER));
+          this.router.navigate(['/mobabuild/search_build']);
+        } else {
+          this.snackBar.open('Credenciales incorrectas', CLOSE, {
+            duration: 3000
+          });
+        }
+      } catch (error) {
+        this.snackBar.open('Credenciales incorrectas', CLOSE, {
+          duration: 3000
+        });
+        console.error('Error during login:', error);
+      }
+    }
   }
 
   cancelar() {
-
+    this.router.navigate(['/mobabuild/search_build']);
   }
 
   createUser(){
-
+    this.router.navigate(['/auth/register']);
   }
 
   togglePasswordVisibility(): void {
