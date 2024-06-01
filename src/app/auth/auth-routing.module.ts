a47edb6531542d62bc4f15e069383543fc2e48b4
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-01 17:11:50 +0200

    auth-routing.module.ts CHANGE routes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomePageComponent,
+    path: '', component: LoginPageComponent,
     children: [
-      { path: 'home_page', component: HomePageComponent },
       { path: 'login', component: LoginPageComponent },
       { path: 'register', component: RegisterPageComponent },
     ]
