6840f9e151697260cf939e287b9a9aa27fd773d5
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-01 13:16:05 +0200

    login-page CHANGE

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from '../../service/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit{
 
+  userForm: FormGroup = new FormGroup({});
+  hidePass: boolean = true;
+
+  constructor(
+    private userService: UserService,
+    private snackBar: MatSnackBar,
+  ) { }
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      pass: new FormControl(null, [Validators.required]),
+    })
+  }
+
+  login() {
+
+  }
+
+  cancelar() {
+
+  }
+
+  createUser(){
+
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePass = !this.hidePass;
+  }
 }
