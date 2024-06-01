6ae942d86c6bac8ac9ae51f811b8d04f8fc28732
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-01 19:14:55 +0200

    home-page.component.ts CHANGES

--- src/app/mobabuild/pages/home-page/home-page.component.ts
+++ src/app/mobabuild/pages/home-page/home-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { User } from 'src/app/shared/interfaces/user';
 import { SharedService } from 'src/app/shared/service/shared.service';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-home-page',
@@ -12,6 +15,17 @@ export class HomePageComponent implements OnInit{
   public menu = { label: 'Menú', icon: 'menu' }
   public isLogedIn = false
 
+  public user: User = {
+    id: 0,
+    email: '',
+    user_name: '',
+    pass: '',
+    image: null,
+    builds: [],
+    favoriteBuild: null,
+    authorities: [],
+  }
+
   public sidebarItems = [
     { label: 'Admin', icon: 'admin_panel_settings', url: '/admin' },
     { label: 'Mobabuild', icon: 'movie', url: '/mobabuild/search_build' },
@@ -21,11 +35,36 @@ export class HomePageComponent implements OnInit{
   constructor(
     private router: Router,
     private sharedService: SharedService,
+    private userService: UserService,
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.router.navigate(['/mobabuild/search_build']);
     this.isLogged();
+    var mayUser = await this.getUser();
+
+    if (mayUser) {
+      this.user = mayUser;
+    }
+  }
+
+  /**
+   * @xavivi8
+   * @description obtiene el usuario por el id en el localStorage
+   * @returns {Promise<User | null>}
+   */
+  async getUser(): Promise<User | null> { // Cambiar el tipo de retorno a `Promise<User | null>`
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      const user: User = JSON.parse(userString);
+      try {
+        return await firstValueFrom(this.userService.findById(user.id));
+      } catch (error) {
+        console.error('Error fetching user by ID:', error);
+        return null; // Manejar el caso de error devolviendo `null`
+      }
+    }
+    return null; // Devuelve `null` si no hay usuario en `localStorage`
   }
 
   /**
