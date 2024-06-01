77fc60d7daa9ab5e6bbc673d174933dc25bb081f
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-01 12:53:53 +0200

    CREATE user.service.ts

---
+++ src/app/auth/service/user.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, catchError, map, of } from "rxjs";
+import { AddUserRequest, User } from "src/app/shared/interfaces/user";
+import { SharedService } from "src/app/shared/service/shared.service";
+import { URL_API } from "src/environments/environments";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private urlMobabuild: string = `${URL_API}/user`;
+
+  constructor(
+    private httpClient: HttpClient,
+    private sharedService: SharedService
+  ) {
+
+  }
+
+  findById(id: number): Observable<User> {
+    return this.httpClient.get<User>(`${this.urlMobabuild}/find/${id}`, this.sharedService.getAuthHeaderWithJson());
+  }
+
+}
