0c61ded2ae61119eab2c659923f063508402d1ef
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-02 18:15:29 +0200

    CREATE fav-build-page.component

---
+++ src/app/mobabuild/pages/fav-build-page/fav-build-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fav-build-page',
+  templateUrl: './fav-build-page.component.html',
+  styleUrls: ['./fav-build-page.component.css']
+})
+export class FavBuildPageComponent {
+
+}
