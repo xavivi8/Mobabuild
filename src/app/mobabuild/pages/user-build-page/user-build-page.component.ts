e398142727ef0416fdfa778799ef4f3af167a0c7
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-02 18:14:50 +0200

    CREATE user-build-page.component

---
+++ src/app/mobabuild/pages/user-build-page/user-build-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-build-page',
+  templateUrl: './user-build-page.component.html',
+  styleUrls: ['./user-build-page.component.css']
+})
+export class UserBuildPageComponent {
+
+}
