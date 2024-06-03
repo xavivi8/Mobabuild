ac139f9c5854d8ac1553705661cf0ec676f4b60c
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-03 16:55:17 +0200

    shared.module.ts ADD declarations

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,14 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { MaterialModule } from '../material/material.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+import { Base64ToImagePipe } from './pipe/base64ToImage.pipe';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    Error404PageComponent
+    Error404PageComponent,
+    Base64ToImagePipe
   ],
   imports: [
     CommonModule,
