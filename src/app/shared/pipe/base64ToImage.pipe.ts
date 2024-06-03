f31b6593e387f919615f21bdc077191ce5ddeb16
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-03 16:54:55 +0200

    CREATE base64ToImage.pipe.ts

---
+++ src/app/shared/pipe/base64ToImage.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'base64ToImage'
+})
+export class Base64ToImagePipe implements PipeTransform {
+
+  transform(base64: string | null | undefined): string {
+    if (!base64) {
+      return '/assets/poro.jpg'; // Imagen por defecto si no hay base64
+    }
+    return `data:image/jpeg;base64,${base64}`;
+  }
+
+}
