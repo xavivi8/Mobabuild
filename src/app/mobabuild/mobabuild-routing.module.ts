564cc6ae4f177e1c8ed6dd9de32bf2c5550b6bb3
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-03 18:43:37 +0200

    DELETE edit build ppage

--- src/app/mobabuild/mobabuild-routing.module.ts
+++ src/app/mobabuild/mobabuild-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AddBuildPageComponent } from './pages/add-build-page/add-build-page.component';
-import { EditBuildPageComponent } from './pages/edit-build-page/edit-build-page.component';
 import { SearchBuildComponent } from './pages/search-build/search-build.component';
 import { ViewBuildPageComponent } from './pages/view-build-page/view-build-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
@@ -14,7 +13,6 @@ const routes: Routes = [
     children: [
       { path: 'home_page', component: HomePageComponent },
       { path: 'add_build', component: AddBuildPageComponent },
-      { path: 'edit_build', component: EditBuildPageComponent },
       { path: 'search_build', component: SearchBuildComponent },
       { path: 'profiel', component: ProfilePageComponent },
       { path: 'user_builds', component: UserBuildPageComponent },
--- src/app/mobabuild/mobabuild.module.ts
+++ src/app/mobabuild/mobabuild.module.ts
@@ -7,7 +7,6 @@ import { MaterialModule } from '../material/material.module';
 import { SearchBuildComponent } from './pages/search-build/search-build.component';
 import { DeleteBuildComponent } from './components/delete-build/delete-build.component';
 import { AddBuildPageComponent } from './pages/add-build-page/add-build-page.component';
-import { EditBuildPageComponent } from './pages/edit-build-page/edit-build-page.component';
 import { SearchBuildCardComponent } from './components/search-build-card/search-build-card.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { ViewBuildPageComponent } from './pages/view-build-page/view-build-page.component';
@@ -24,7 +23,6 @@ import { MatTableModule } from '@angular/material/table';
     SearchBuildComponent,
     DeleteBuildComponent,
     AddBuildPageComponent,
-    EditBuildPageComponent,
     SearchBuildCardComponent,
     ViewBuildPageComponent,
     Base64ToImagePipe,
