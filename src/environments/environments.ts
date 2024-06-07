75b08a236d27fea83ccb867fdca47b9036f09d37
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-07 19:29:46 +0200

    ADD routes

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,2 +1,3 @@
-export const URL_BASE = 'http://localhost:8080';
+export const URL_BASE = 'http://134.209.253.226:8080';
+//export const URL_BASE = 'http://localhost:8080';
 export const URL_API = `${URL_BASE}/api`;
