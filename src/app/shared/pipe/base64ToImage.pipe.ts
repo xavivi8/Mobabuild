efc96e73aef333089eefe5703e079581e1ad18fa
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-06-12 07:52:09 +0200

    CHANGE image route

--- src/app/shared/pipe/base64ToImage.pipe.ts
+++ src/app/shared/pipe/base64ToImage.pipe.ts
@@ -7,7 +7,7 @@ export class Base64ToImagePipe implements PipeTransform {
 
   transform(base64: string | null | undefined): string {
     if (!base64) {
-      return '/assets/poro.jpg'; // Imagen por defecto si no hay base64
+      return 'assets/poro.jpg'; // Imagen por defecto si no hay base64
     }
     return `data:image/jpeg;base64,${base64}`;
   }
